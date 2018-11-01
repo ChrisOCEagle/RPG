c2a89da13e5ec2707362089b6f8f3509d3e65e9c
Siberianeagle5 <christopherdavyydoconnell.com>
2018-10-31 22:46:52 -0400

    so far the creator constructor only gives the character a name and a class

--- characterCreator.js
+++ characterCreator.js
@@ -9,15 +9,6 @@ function CharacterCreator(name, character_class) {
     this.name = name;
     // this will represent the class of the character which the player gets to choose
     this.class = character_class;
-    // this will represent the stats for the character
-    this.stats = {
-        Strength: statRoller(),
-        Dexterity: statRoller(),
-        Constitution: statRoller(),
-        Intelligence: statRoller(),
-        Wisdom: statRoller(),
-        Charisma: statRoller()
-    };
 };
 
 module.exports = CharacterCreator;
--- game.js
+++ game.js
@@ -1,3 +1,3 @@
 const CharacterCreator = require("./characterCreator.js");
-var mage = new CharacterCreator();
+var mage = new CharacterCreator("Chris", "mage");
 console.log(mage);
