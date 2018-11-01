30c8a503fa3482159cc36008ecfcc7740a2bd73c
Siberianeagle5 <christopherdavyydoconnell.com>
2018-10-31 21:54:10 -0400

    added the capability to add a name and class to the character. Also added stats to the character.

--- characterCreator.js
+++ characterCreator.js
@@ -3,7 +3,21 @@ var statRoller = require("./statRoller.js");
 const DiceRoller = require("./DiceRoller.js");
 var diceRoller = new DiceRoller();
 
-function CharacterCreator() {
+// this is the character creator constructor that will create a character object
+function CharacterCreator(name, character_class) {
+    // this will represent the name of the character which the player gets to choose
+    this.name = name;
+    // this will represent the class of the character which the player gets to choose
+    this.class = character_class;
+    // this will represent the stats for the character
+    this.stats = {
+        Strength: statRoller(),
+        Dexterity: statRoller(),
+        Constitution: statRoller(),
+        Intelligence: statRoller(),
+        Wisdom: statRoller(),
+        Charisma: statRoller()
+    };
 };
 
 module.exports = CharacterCreator;
