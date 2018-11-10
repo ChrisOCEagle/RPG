f82e6edce95914a46bcf7035eaf64fb0d72e6e51
Siberianeagle5 <christopherdavyydoconnell.com>
2018-11-10 18:10:04 -0500

    still working on the characterCreator constructor

--- characterCreator.js
+++ characterCreator.js
@@ -26,26 +26,68 @@ function CharacterCreator(name, character_class) {
     // whether they have magicka or stamina and the total points of each,
     // their hit dice total, and the type of magicka dice or stamina dice as well
     if (this.class === "warrior") {
-        this.hitDice = diceRoller.d12;
-        this.staminaDice = diceRoller.d6;
-        this.hitPoints = this.hitDice() + this.stats.Constitution.modifier;
-        this.staminaPoints = this.staminaDice() + this.stats.Constitution.modifier;
+        var hitDice = diceRoller.d12;
+        var hitPoints = hitDice() + this.stats.Constitution.modifier;
     };
     if (this.class === "rogue") {
-        this.hitDice = diceRoller.d8;
-        this.staminaDice = diceRoller.d10;
-        this.hitPoints = this.hitDice() + this.stats.Constitution.modifier;
-        this.staminaPoints = this.staminaDice() + this.stats.Constitution.modifier;
+        var hitDice = diceRoller.d8;
+        var hitPoints = hitDice() + this.stats.Constitution.modifier;
     };
     if (this.class === "mage") {
-        this.hitDice = diceRoller.d6;
-        this.magickaDice = diceRoller.d12;
-        this.hitPoints = this.hitDice() + this.stats.Constitution.modifier;
-        this.magickaPoints = this.magickaDice() + this.stats.Intelligence.modifier;
+        var hitDice = diceRoller.d6;
+        var hitPoints = hitDice() + this.stats.Constitution.modifier;
     };
+    this.hitDice = hitDice;
+    // ---------------------------------------------
+    this.hitPoints = hitPoints;
+    // ---------------------------------------------
     // the character's level tracks how powerful the character is
     // and increments along with other character properties upon level up
     this.level = 0;
+    this.levelUp = function() {
+        console.log("Level Up!");
+        this.level++;
+        var choices = Object.entries(this.stats);
+        var hitDice = this.hitDice;
+        var hitPoints = this.hitPoints;
+        inquirer.prompt(
+            [
+                {
+                    type: "list",
+                    name: "stat-1",
+                    message: "What stat would you like to increase?",
+                    choices: Object.keys(this.stats)
+                },
+                {
+                    type: "list",
+                    name: "stat-2",
+                    message: "What stat would you like to increase?",
+                    choices: Object.keys(this.stats)
+                }
+            ]
+        ).then(function(answers) {
+            for (let i = 0; i < choices.length; i++) {
+                if (answers['stat-1'] === answers['stat-2']) {
+                    if (choices[i][0] === answers['stat-1']) {
+                        choices[i][1].score += 2;
+                        choices[i][1].modifier = Math.floor((choices[i][1].score - 10) / 2);
+                    };
+                } else if (choices[i][0] === answers['stat-1'] || choices[i][0] === answers['stat-2']) {
+                    choices[i][1].score++;
+                    choices[i][1].modifier = Math.floor((choices[i][1].score - 10) / 2);
+                }
+                this.stats = choices
+            };
+            for (let i = 0; i < this.stats.length; i++) {
+                if (this.stats[i][0] === "Constitution") {
+                    hitPoints += hitDice() + this.stats[i][1].modifier;
+                    // --------------------------------------------------
+                    this.hitPoints = hitPoints;
+                    // --------------------------------------------------
+                };
+            };
+        });   
+    };
 };
 
 module.exports = CharacterCreator;
--- game.js
+++ game.js
@@ -4,8 +4,12 @@ var mage = new CharacterCreator("Kyle", "mage");
 var warrior = new CharacterCreator("Sara", "warrior");
 var divider = "\n----------------------------------------\n";
 console.log(rogue);
-console.log(divider);
-console.log(mage);
-console.log(divider);
-console.log(warrior);
-//rogue.levelUp();
+//console.log(divider);
+// console.log(mage);
+// console.log(divider);
+// console.log(warrior);
+rogue.levelUp();
+setTimeout(function() {
+    console.log(divider);
+    console.log(rogue);
+}, 5000)
